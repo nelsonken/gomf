a9dd2f9deae94b4d390d6557c898dd435cbe04ed
ken <31346910+nelsonken@users.noreply.github.com>
2018-05-04 20:39:37 +0800

    Update up.php

--- up.php
+++ up.php
@@ -17,15 +17,14 @@ if ($_FILES["picture"]["error"] > 0) {
     echo "Size: " . ($_FILES["picture"]["size"] / 1024) . " Kb\n";
     echo "Temp file: " . $_FILES["picture"]["tmp_name"] . "\n>";
 
-    if (file_exists("upload/" . $_FILES["picture"]["name"]))
+    if (file_exists($_FILES["picture"]["name"]))
       {
       echo $_FILES["picture"]["name"] . " already exists. \n";
       }
     else
       {
-      move_uploaded_file($_FILES["picture"]["tmp_name"],
-      "upload/" . $_FILES["picture"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["picture"]["name"] . "\n";
+      move_uploaded_file($_FILES["picture"]["tmp_name"], $_FILES["picture"]["name"]);
+      echo "Stored in: " . $_FILES["picture"]["name"] . "\n";
       }
 }
 
